ac07184026a1d851c6e9fa1729315f587f4299c8
gontiago92 <tiago.goncalves.faria@gmail.com>
2022-12-30 23:46:56 +0100

    - Added code from Lesson 1 to 14 from the Mastering Nuxt 3 Course

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  videoId: number;
+}>();
+</script>
+
+<template>
+  <iframe
+      width="560"
+      height="315"
+      :src="`https://player.vimeo.com/video/${props.videoId}`"
+      title="Video player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+  ></iframe>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Page: Hello, Mastering Nuxt 3 !
+  </div>
+</template>
+
+<style scoped></style>
